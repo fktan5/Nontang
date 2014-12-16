aa5d6de0c2f23924d607277c531b3c7d6d66ad7a
fktan5 <email@fktan5.net>
2014-12-16 09:12:25 +0000

    骨格作り 対応するクエリだけ追加

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
-  puts "IT WORKS!"
+  'IT WORKS!'
+end
+
+get '/member' do
+  member = { name: "Name" }
+
+  member.to_json
+end
+
+get '/story' do
+  story = { body: "Body" }
+  story.to_json
+end
+
+get '/demo' do
+  nozomi = Pebbles::LoveLive.nozomi
+  nozomi.name
 end
