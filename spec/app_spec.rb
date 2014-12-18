32de87e1b9e8156164d8f10fec605619d617fbff
fktan5 <email@fktan5.net>
2014-12-18 06:19:22 +0000

    Rspecの書き方修正

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -15,36 +15,35 @@ describe 'The HelloWorld App' do
   it "says itworks" do
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq('IT WORKS!')
   end
 
   it "says story" do
     get '/story'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 
   it "says story with param" do
     get '/story?season=1&num=2'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 
   it "says story with bad param" do
     get '/story?nicopuri=joshido'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 
   it "says member(No param)" do
     get '/member'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 
   it "says member(with param)" do
     get '/member?name=honoka'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 
   it "says member(with bad param)" do
     get '/member?wrong=param'
-    last_response.should be_ok
+    expect(last_response).to be_ok
   end
 end
